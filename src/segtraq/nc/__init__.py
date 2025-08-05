a6f172d4bbeb7bfd02b50e154a9ad1e55f8f3482
Daria Lazic <lazic@login2.cluster.embl.de>
2025-08-05 09:36:50 +0200

    Initial commit to feature/corr_cell_nucleus

---
+++ src/segtraq/nc/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for SegTraQ."""
+
+__author__ = """Daria Lazic, Matthias Meyer-Bender, Martin Emons"""
+__email__ = "daria.lazic@embl.de, matthias.meyerbender@embl.de, martin.emons@uzh.ch"
+
+from . import nc
+
+__all__ = ["nc"]
