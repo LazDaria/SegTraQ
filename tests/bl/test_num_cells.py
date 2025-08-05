b8b15c423b173a22f4d84304b7753c970eed9eb5
Matthias Fabian Meyer-Bender <matthias.meyerbender@embl.de>
2025-08-05 16:00:29 +0200

    implemented basic testing framework

--- tests/bl/test_num_cells.py
+++ tests/bl/test_num_cells.py
@@ -1,2 +1,13 @@
-def test_num_cells():
-    pass
+import segtraq as st
+import pytest
+
+def test_num_cells(sdata):
+    num_cells = st.bl.num_cells(sdata)
+    assert num_cells == 138, f"Expected 3 cells, found {num_cells}"
+    
+    num_nuclei = st.bl.num_cells(sdata, shape_key="nucleus_boundaries")
+    assert num_nuclei == 123, f"Expected 3 cells, found {num_cells}"
+
+def test_num_cells_invalid_key(sdata):
+    with pytest.raises(KeyError):
+        st.bl.num_cells(sdata, shape_key="invalid_key")
