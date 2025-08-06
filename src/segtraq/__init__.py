1f7103d569fe0776ea5a3b61610902b708bdb8c4
Matthias Fabian Meyer-Bender <matthias.meyerbender@embl.de>
2025-08-06 11:41:28 +0200

    added clustering stability (cs) module

--- src/segtraq/__init__.py
+++ src/segtraq/__init__.py
@@ -3,6 +3,6 @@
 __author__ = """Daria Lazic, Matthias Meyer-Bender, Martin Emons"""
 __email__ = "daria.lazic@embl.de, matthias.meyerbender@embl.de, martin.emons@uzh.ch"
 
-from . import bl
+from . import bl, cs
 
-__all__ = ["bl"]
+__all__ = ["bl", "cs"]
---
+++ src/segtraq/cs/__init__.py
@@ -0,0 +1,3 @@
+from .clustering_stability import compute_clustering_stability
+
+__all__ = ["compute_clustering_stability"]
