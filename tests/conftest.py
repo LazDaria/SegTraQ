50159ea22606773d514ff797062fac77b4237206
LazDaria <daria_lazic@yahoo.com>
2025-08-07 16:14:11 +0200

    test proseg

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,6 @@ from spatialdata import SpatialData
 @pytest.fixture(scope="session", name="sdata_new")
 def test_sdata_new():
     """Load the SpatialData test sample once per test session."""
-    test_data_path = Path(__file__).parent / "data" / "test_sample.zarr"
+    test_data_path = Path(__file__).parent / "data" / "spatialData_2_C1_proseg.zarr"
     sdata_new = SpatialData.read(test_data_path)
     return sdata_new
