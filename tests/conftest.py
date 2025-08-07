7574c27668d2eec4402568c1b30a69191b3e2cd2
LazDaria <daria_lazic@yahoo.com>
2025-08-07 15:26:35 +0200

    update

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-
 import pytest
 from spatialdata import SpatialData
 
@@ -7,6 +5,7 @@ from spatialdata import SpatialData
 @pytest.fixture(scope="session", name="sdata")
 def test_sdata():
     """Load the SpatialData test sample once per test session."""
-    test_data_path = Path(__file__).parent / "data" / "test_sample.zarr"
+    # test_data_path = Path(__file__).parent / "data" / "test_sample.zarr"
+    test_data_path = "/g/huber/projects/CODEX/segtraq/data/xenium_sp_subset.zarr"
     sdata = SpatialData.read(test_data_path)
     return sdata
