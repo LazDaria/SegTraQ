e4347351ba6ba662244398de08a8c23e085e328d
Matthias Fabian Meyer-Bender <matthias.meyerbender@embl.de>
2025-08-07 15:48:28 +0200

    updated sdata name in fixture

--- tests/bl/test_num_transcripts.py
+++ tests/bl/test_num_transcripts.py
@@ -3,12 +3,12 @@ import pytest
 import segtraq as st
 
 
-def test_num_transcripts(sdata):
-    num_transcripts = st.bl.num_transcripts(sdata)
+def test_num_transcripts(sdata_new):
+    num_transcripts = st.bl.num_transcripts(sdata_new)
     assert type(num_transcripts) is int, "Number of transcripts should be an integer"
     assert num_transcripts > 0, "Number of transcripts should be greater than zero"
 
 
-def test_num_transcripts_invalid_key(sdata):
+def test_num_transcripts_invalid_key(sdata_new):
     with pytest.raises(KeyError):
-        st.bl.num_transcripts(sdata, transcript_key="invalid_key")
+        st.bl.num_transcripts(sdata_new, transcript_key="invalid_key")
--- tests/nc/test_compute_cell_nuc_correlation.py
+++ tests/nc/test_compute_cell_nuc_correlation.py
@@ -4,9 +4,9 @@ import pytest
 import segtraq as st
 
 
-def test_data_types_and_columns(sdata):
+def test_data_types_and_columns(sdata_new):
     # Prepare table.obs with best_nuc_id and IoU
-    corr_df = st.nc.compute_cell_nuc_correlation(sdata)
+    corr_df = st.nc.compute_cell_nuc_correlation(sdata_new)
     assert isinstance(corr_df, pd.DataFrame), f"Expected DataFrame, got {type(corr_df)}"
     assert set(corr_df.columns) == {
         "cell_id",
@@ -19,6 +19,6 @@ def test_data_types_and_columns(sdata):
     )
 
 
-def test_unsupported_metric_raises_value_error(sdata):
+def test_unsupported_metric_raises_value_error(sdata_new):
     with pytest.raises(ValueError, match="Metric spearman not supported"):
-        st.nc.compute_cell_nuc_correlation(sdata, metric="spearman")
+        st.nc.compute_cell_nuc_correlation(sdata_new, metric="spearman")
