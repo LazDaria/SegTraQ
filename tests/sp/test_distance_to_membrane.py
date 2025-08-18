e3d8c403596a28c327ddc3d5b03a4832458aa884
mjemons <martin.emons@gmail.com>
2025-08-18 14:51:50 +0200

    fixing test

--- tests/sp/test_distance_to_membrane.py
+++ tests/sp/test_distance_to_membrane.py
@@ -7,7 +7,7 @@ def test_distance_to_membrane_structure(sdata_new):
     df = st.sp.distance_to_membrane(sdata_new, feature="LUM")
 
     assert isinstance(df, pd.DataFrame), f"centroid_mean_coord_diff should return a DataFrame, got {type(df)}"
-    expected_cols = {"distance_to_outline_inverse", "distance_to_outline", "cell_area"}
+    expected_cols = {"distance_to_outline_inverse", "distance_to_outline", "cell_area", "cell_id"}
     assert set(df.columns) == expected_cols, f"Expected columns {expected_cols}, but got {set(df.columns)}"
 
 def test_distance_to_membrane_structure_values(sdata_new):
