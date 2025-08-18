6e040010a4fd8e9f60ee1ed6179ed6d8f46a587e
mjemons <martin.emons@gmail.com>
2025-08-18 14:47:22 +0200

    changed variable name for tests

--- tests/sp/test_distance_to_membrane.py
+++ tests/sp/test_distance_to_membrane.py
@@ -3,14 +3,14 @@ import pandas as pd
 import segtraq as st
 
 
-def test_distance_to_membrane_structure(sdata):
-    df = st.sp.distance_to_membrane(sdata, feature="LUM")
+def test_distance_to_membrane_structure(sdata_new):
+    df = st.sp.distance_to_membrane(sdata_new, feature="LUM")
 
     assert isinstance(df, pd.DataFrame), f"centroid_mean_coord_diff should return a DataFrame, got {type(df)}"
     expected_cols = {"distance_to_outline_inverse", "distance_to_outline", "cell_area"}
     assert set(df.columns) == expected_cols, f"Expected columns {expected_cols}, but got {set(df.columns)}"
 
-def test_distance_to_membrane_structure_values(sdata):
-    df = st.sp.distance_to_membrane(sdata, feature="LUM")
+def test_distance_to_membrane_structure_values(sdata_new):
+    df = st.sp.distance_to_membrane(sdata_new, feature="LUM")
 
     assert sum(df["distance_to_outline"] < df["cell_area"]) == df.shape[0], f"The distance metric is not always smaller than the area"
