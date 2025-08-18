b35091529380676e11317cf986c665f5a4c5d3a9
Martin Emons <55688661+mjemons@users.noreply.github.com>
2025-08-18 14:58:19 +0200

    function to compute centroid-meantranscriptcoordinate difference (#14)
    * function to compute centroid-meantranscriptcoordinate difference
    
    * first few tests for the centroid difference method
    
    * added new module
    
    * implemented distance to cell outline - TODO: testing
    
    * fixed a bug in distance_to_membrane and implemented tests for this function
    
    * Delete src/segtraq/sp/__main__.py
    
    * changed variable name for tests
    
    * fixing test
    
    ---------
    
    Co-authored-by: mjemons <martin.emons@gmail.com>

---
+++ tests/sp/test_centroid_mean_coord_diff.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+import segtraq as st
+
+
+def test_centroid_mean_coord_diff_structure(sdata_new):
+    df = st.sp.centroid_mean_coord_diff(sdata_new, feature="LUM")
+
+    assert isinstance(df, pd.DataFrame), f"centroid_mean_coord_diff should return a DataFrame, got {type(df)}"
+    expected_cols = {"cell_id", "centroid_x", "centroid_y", "x", "y", "distance", "cell_area"}
+    assert set(df.columns) == expected_cols, f"Expected columns {expected_cols}, but got {set(df.columns)}"
+
+def test_centroid_mean_coord_diff_values(sdata_new):
+    df = st.sp.centroid_mean_coord_diff(sdata_new, feature="LUM")
+
+    assert sum(df["distance"] < df["cell_area"]) == df.shape[0], f"The distance metric is not always smaller than the area"
