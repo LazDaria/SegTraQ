983596e3629fefd30ceaed374e39bf626a898c7a
LazDaria <daria_lazic@yahoo.com>
2025-11-05 15:45:04 +0100

    Bug fixes in SegTraQ.py

--- src/segtraq/__init__.py
+++ src/segtraq/__init__.py
@@ -3,8 +3,8 @@
 __author__ = """Daria Lazic, Matthias Meyer-Bender, Martin Emons"""
 __email__ = "daria.lazic@embl.de, matthias.meyerbender@embl.de, martin.emons@uzh.ch"
 
-from . import bl, cs, io, nc, pl, sp
+from . import bl, cs, nc, pl, sp
 from .utils import run_label_transfer, validate_spatialdata
 from .SegTraQ import SegTraQ
 
-__all__ = ["bl", "cs", "nc", "io", "sp", "pl", "segtraq_metrics", "run_label_transfer", "validate_spatialdata", "SegTraQ"]
+__all__ = ["bl", "cs", "nc", "sp", "pl", "segtraq_metrics", "run_label_transfer", "validate_spatialdata", "SegTraQ"]
