88dcec29d0fb014d174b9d05e18de56bb99d2a4a
Matthias Fabian Meyer-Bender <matthias.meyerbender@embl.de>
2025-11-05 14:15:29 +0100

    removed technology-specific readers in favor of a simple validate_spatialdata() function

--- src/segtraq/io/__init__.py
+++ src/segtraq/io/__init__.py
@@ -1,12 +1,5 @@
-from .io import read_bidcell, read_proseg_2, read_proseg_3, read_segger, read_xenium
-from .utils import create_spatialdata, validate_spatialdata
+from .io import validate_spatialdata
 
 __all__ = [
-    "create_spatialdata",
     "validate_spatialdata",
-    "read_xenium",
-    "read_bidcell",
-    "read_segger",
-    "read_proseg_2",
-    "read_proseg_3",
 ]
