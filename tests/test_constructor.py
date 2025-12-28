4a137840d736aa483addc36d36a53b63f800a033
LazDaria <daria_lazic@yahoo.com>
2025-12-28 16:38:17 +0100

    Renamed tables_x_key to tables_centroid_x_key

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -4,9 +4,9 @@ import segtraq as st
 
 
 def test_constructor(sdata_new):
-    st.SegTraQ(sdata_new, tables_x_key="x_centroid", tables_y_key="y_centroid", images_key="image")
+    st.SegTraQ(sdata_new, tables_centroid_x_key="x_centroid", tables_centroid_y_key="y_centroid", images_key="image")
 
 
 def test_constructor_invalid_coordinate(sdata_new):
     with pytest.raises(AssertionError, match="Tables DataFrame must contain x coordinate column"):
-        st.SegTraQ(sdata_new, images_key="image")
+        st.SegTraQ(sdata_new, images_key="image", tables_centroid_x_key="x", tables_centroid_y_key="y")
