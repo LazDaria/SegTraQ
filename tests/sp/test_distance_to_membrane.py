3619463167924ebc36fed75473dd07bd80a8e1cb
mjemons <martin.emons@gmail.com>
2026-01-20 17:03:11 +0100

    adapted tests

--- tests/sp/test_distance_to_membrane.py
+++ tests/sp/test_distance_to_membrane.py
@@ -7,5 +7,5 @@ def test_distance_to_membrane_structure(sdata_new):
     df = st.ps.distance_to_membrane(sdata_new, genes="LUM", inplace=False)
 
     assert isinstance(df, pd.DataFrame), f"centroid_mean_coord_diff should return a DataFrame, got {type(df)}"
-    expected_cols = {"cell_area", "distance_to_outline_inverse_LUM", "cell_id", "distance_to_outline_LUM"}
+    expected_cols = {"cell_id", "distance_to_outline_aggregated_LUM", "cell_area", "distance_to_outline_norm_aggregated_LUM", "distance_to_outline_inverse_aggregated_LUM"}
     assert set(df.columns) == expected_cols, f"Expected columns {expected_cols}, but got {set(df.columns)}"
