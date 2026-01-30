28539a3a5dd5d109fa64439d7922c115caefea2c
mjemons <martin.emons@gmail.com>
2026-01-30 11:03:04 +0100

    removed test of shapes due to filtering

--- tests/vl/test_compute_sim_top_bottom_z.py
+++ tests/vl/test_compute_sim_top_bottom_z.py
@@ -9,13 +9,6 @@ def test_compute_sim_top_bottom_z_type(sdata_new):
     expected_cols = {"cell_id", "cosine_sim_top_bottom_z"}
     assert set(df.columns) == expected_cols, f"Expected columns {expected_cols}, but got {df.columns}"
     
-def test_compute_sim_top_bottom_z_shapes(sdata_new):
-    df = st.vl.compute_sim_top_bottom_z(sdata_new)
-    
-    n_cells = sdata_new.tables["table"].obs["cell_id"].unique()
-    
-    assert len(n_cells) <= len(df["cell_id"].unique())
-    
 def test_compute_sim_top_bottom_z_values(sdata_new):
     df = st.vl.compute_sim_top_bottom_z(sdata_new)
     
