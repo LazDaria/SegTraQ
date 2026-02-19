539723c9a199d715023c32120da1006bd15bf7be
Matthias Fabian Meyer-Bender <matthias.meyerbender@embl.de>
2026-02-19 15:51:24 +0100

    improved warnings

--- src/segtraq/utils.py
+++ src/segtraq/utils.py
@@ -1044,7 +1044,7 @@ def validate_spatialdata(
     assert points_key in sdata.points, (
         f"SpatialData must contain points with key: {points_key}. "
         f"Available keys: {list(sdata.points.keys())}. "
-        f"If you want to use a different key, set the points_key parameter."
+        f"You can set this with the 'points_key' argument."
     )
     points = sdata.points[points_key]
 
@@ -1052,7 +1052,7 @@ def validate_spatialdata(
     assert points_cell_id_key in points.columns, (
         f"Points DataFrame must contain column to identify cells: {points_cell_id_key}. "
         f"Available columns: {points.columns.tolist()}. "
-        f"If you want to use a different column, set the points_cell_id_key parameter."
+        f"You can set this with the 'points_cell_id_key' argument."
     )
 
     # check coordinate columns in points
@@ -1092,7 +1092,7 @@ def validate_spatialdata(
             assert shapes_key in sdata.shapes, (
                 f"Shapes DataFrame must contain key: {shapes_key}. "
                 f"Available keys: {list(sdata.shapes.keys())}. "
-                f"If you want to use a different key, set the shapes_key parameter."
+                f"If you want to use a different key, you can set this with the 'shapes_key' argument."
             )
             shapes = sdata.shapes[shapes_key]
 
@@ -1130,7 +1130,7 @@ def validate_spatialdata(
         if points_background_id is not None:
             assert points_background_id in transcript_ids, (
                 f"points_background_id '{points_background_id}' not found among point cell IDs. "
-                f"You can set this with the points_background_id parameter. "
+                f"You can set this with the 'points_background_id' argument. "
                 f"If you do not have a background ID, set this parameter to None."
             )
 
@@ -1153,13 +1153,13 @@ def validate_spatialdata(
             assert tables_key in sdata.tables, (
                 f"Tables DataFrame must contain key: {tables_key}. "
                 f"Available keys: {list(sdata.tables.keys())}. "
-                f"If you want to use a different key, set the tables_key parameter."
+                f"If you want to use a different key, set the 'tables_key' parameter."
             )
             table = sdata.tables[tables_key]
             assert tables_cell_id_key in table.obs.columns, (
                 f"Tables DataFrame must contain column: {tables_cell_id_key}. "
                 f"Available columns: {table.obs.columns.tolist()}. "
-                f"If you want to use a different column, set the tables_cell_id_key parameter."
+                f"If you want to use a different column, set the 'tables_cell_id_key' parameter."
             )
 
             assert "spatialdata_attrs" in table.uns, "Could not find 'spatialdata_attrs' in table.uns. "
@@ -1247,6 +1247,8 @@ def validate_spatialdata(
             raise KeyError(
                 f"Nucleus shapes key '{nucleus_shapes_key}' not found in sdata.shapes. "
                 f"Available keys: {list(sdata.shapes.keys())}."
+                f"You can set this with the 'nucleus_shapes_key' argument. "
+                f"Set to None if you do not have nucleus shapes."
             )
 
         nucleus_shapes = sdata.shapes[nucleus_shapes_key]
