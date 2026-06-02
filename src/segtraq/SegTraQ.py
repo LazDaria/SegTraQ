1ddcd90abc9839c057329b125e8961e56084a3a8
LazDaria <daria_lazic@yahoo.com>
2026-06-02 18:53:43 +0200

    Bugfix in markers_from_reference.

--- src/segtraq/SegTraQ.py
+++ src/segtraq/SegTraQ.py
@@ -7,7 +7,7 @@ import spatialdata as sd
 from anndata import AnnData
 
 from . import bl, cs, pl, ps, rs, sp, vl
-from .utils import _filter_control_and_low_quality_transcripts, _get_genes, validate_spatialdata
+from .utils import _filter_control_and_low_quality_transcripts, _get_genes, validate_spatialdata, _make_ref_genes_unique
 from .utils import filter_cells as _filter_cells
 from .utils import markers_from_reference as _markers_from_reference
 from .utils import run_label_transfer as _run_label_transfer
@@ -957,6 +957,7 @@ class SegTraQ:
     ):
         sp_genes = _get_genes(adata=self.sdata.tables[self.tables_key], gene_key=self.tables_gene_key)
 
+        adata = _make_ref_genes_unique(adata=adata, gene_key=ref_gene_key)
         sc_genes = _get_genes(adata=adata, gene_key=ref_gene_key)
 
         mask = sc_genes.isin(sp_genes)
