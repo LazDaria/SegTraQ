9d9e5da3aab55c3304953012676e908c066847c3
Matthias Fabian Meyer-Bender <matthias.meyerbender@embl.de>
2026-07-22 13:44:35 +0200

    made X the default to check for raw counts

--- tests/sp/test_neighbor_contamination.py
+++ tests/sp/test_neighbor_contamination.py
@@ -4,7 +4,7 @@ import segtraq as st
 
 
 def test_neighbor_contamination_realdata_inplace_outputs(sdata_labeled, adata_ref):
-    markers = st.markers_from_reference(adata_ref.copy(), ref_cell_type="celltype")
+    markers = st.markers_from_reference(adata_ref.copy(), ref_cell_type="celltype", ref_raw_counts_layer="raw")
 
     (
         per_cell_df,
